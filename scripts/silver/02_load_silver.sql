d02a72c7b19cfbb54228e16935ced6b0ef7d596c
StaIkussia <aimansadiev06@gmail.com>
2026-06-05 12:40:58 +0600

    feat: add silver load script for crm_prd_info transformation

--- scripts/silver/02_load_silver.sql
+++ scripts/silver/02_load_silver.sql
@@ -72,3 +72,26 @@ FROM (
 	AND cst_key IS NOT NULL
 ) AS dedup
 WHERE rn = 1;
+
+TRUNCATE TABLE silver.crm_prd_info;
+INSERT INTO silver.crm_prd_info (
+    prd_id, cat_id, prd_key, prd_nm, prd_cost,
+    prd_line, prd_start_dt, prd_end_dt
+)
+SELECT
+    prd_id,
+    REPLACE(SUBSTRING(prd_key FROM 1 FOR 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key FROM 7) AS prd_key,
+	prd_nm,
+    prd_cost,
+    CASE UPPER(TRIM(prd_line))
+	    WHEN 'M' THEN 'Mountain'
+	    WHEN 'R' THEN 'Road'
+	    WHEN 'S' THEN 'Other Sales'
+	    WHEN 'T' THEN 'Touring'
+	    ELSE 'Unknown'
+    END,
+    prd_start_dt,
+    LEAD(prd_start_dt) 
+    OVER(PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day' AS prd_end_dt
+FROM bronze.crm_prd_info;
