e400e047e3e3c4910039c570b126cba1e66026fc
StaIkussia <aimansadiev06@gmail.com>
2026-06-16 10:35:40 +0600

    fix: fix minor typo

--- scripts/gold/01_ddl_gold_views.sql
+++ scripts/gold/01_ddl_gold_views.sql
@@ -81,4 +81,4 @@ FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 	ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
-	ON sd.sls_cust_id = cu.customer_id;= la.cid;
+	ON sd.sls_cust_id = cu.customer_id;
